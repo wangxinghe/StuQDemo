239452d40fe698d90085ccfad8c42b7046da72e8
wangxinghe <wangxinghe1988@163.com>
2016-11-23 02:18:01 +0800

    update

--- app/src/main/java/com/mouxuejie/stuqdemo/view/WatchView.java
+++ app/src/main/java/com/mouxuejie/stuqdemo/view/WatchView.java
@@ -12,7 +12,7 @@ import android.view.View;
  */
 
 public class WatchView extends View {
-    private static final int DEFAULT_SIZE = 300;
+    private static final int DEFAULT_SIZE = 1000;
 
     private Paint mPaint;
     private Paint mLinePaint;
@@ -35,6 +35,7 @@ public class WatchView extends View {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        //处理LayoutParams为wrap_content的情况,因为lp为wrap_content对应的specMode为相当于
         int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
         int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
         int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
@@ -51,15 +52,23 @@ public class WatchView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        int width = getMeasuredWidth();
-        int height = getMeasuredHeight();
+        int paddingLeft = getPaddingLeft();
+        int paddingRight = getPaddingRight();
+        int paddingTop = getPaddingTop();
+        int paddingBottom = getPaddingBottom();
+        int width = getMeasuredWidth() - paddingLeft - paddingRight;
+        int height = getMeasuredHeight() - paddingTop - paddingBottom;
+
+        //中心坐标,半径
         int radius = Math.min(width, height) / 2;
+        int cx = width / 2 + paddingLeft;
+        int cy = height / 2 + paddingTop;
 
         //画表盘
-        canvas.drawCircle(width / 2, height / 2, radius, mPaint);
+        canvas.drawCircle(cx, cy, radius, mPaint);
 
         //画时间线
-        canvas.drawLine(width / 2, height / 2 - radius / 2, width / 2, height / 2 - radius, mLinePaint);
+        canvas.drawLine(cx, cy - radius / 2, cx, cy - radius, mLinePaint);
         for (int i = 0; i < 11; i++) {
             //i == 0 和i == 1时分别暂存Canvas状态,结果不同
 //            if (i == 0) {
@@ -68,16 +77,16 @@ public class WatchView extends View {
             if (i == 1) {
                 canvas.save();
             }
-            canvas.rotate(30, width / 2, height / 2);
-            canvas.drawLine(width / 2, height / 2 - radius / 2, width / 2, height / 2 - radius, mLinePaint);
+            canvas.rotate(30, cx, cy);
+            canvas.drawLine(cx, cy - radius / 2, cx, cy - radius, mLinePaint);
         }
 
         //恢复到save暂存时的Canvas状态
         canvas.restore();
 
         //Canvas再旋转15度
-        canvas.rotate(15, width / 2, height / 2);
-        canvas.drawLine(width / 2, height / 2 - radius / 2, width / 2, height / 2 - radius, mPaint);
+        canvas.rotate(15, cx, cy);
+        canvas.drawLine(cx, cy - radius / 2, cx, cy - radius, mPaint);
     }
 
     private void initPaint() {
@@ -85,12 +94,12 @@ public class WatchView extends View {
         mPaint.setColor(Color.RED);
         mPaint.setAntiAlias(true);
         mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setStrokeWidth(10f);
+        mPaint.setStrokeWidth(4f);
 
         mLinePaint = new Paint();
         mLinePaint.setColor(Color.BLUE);
         mLinePaint.setAntiAlias(true);
         mLinePaint.setStyle(Paint.Style.FILL);
-        mLinePaint.setStrokeWidth(10f);
+        mLinePaint.setStrokeWidth(4f);
     }
 }
